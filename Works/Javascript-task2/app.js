b82665b10d76eb243ff9ad9bbf7f7241b2d0af25
Gulcin Mustafazade <gulcinmustafazada@gmail.com>
2021-11-30 22:25:07 +0400

    Javascript task 2 and task 3 is completed

---
+++ Works/Javascript-task2/app.js
@@ -0,0 +1,8 @@
+console.log(3+(2-true+false)/3&&true); //nəticə true olacaq.
+//bu kodun izahi aşağıdakı kimdir.
+console.log(2-true+false); /* ilk olaraq mötərizənin içi hell olunur və
+ burada avtomatik Number tipinə çevirməyə çalışdığına görə netice 2-1+0=1 olacaq */
+ 
+console.log(3+(1/3)) // daha sonfra bölmə əməliyyatı icra olunur və burada  nəticə 10/3 olacaq
+
+console.log(10/3&&true)//burada netice her iki terefi true goturduyune gore netice true olacaq
