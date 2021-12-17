d45f86efb56ad4b583a962670aace7db0c59e9ec
Gulcin Mustafazade <gulcinmustafazada@gmail.com>
2021-12-17 23:58:37 +0400

    javascript tasks is updated

---
+++ Works/Javascript-Day07-task/js/action.js
@@ -0,0 +1,18 @@
+let element=document.querySelector('body')
+function changeBgColor(){
+    element.style.background="red"
+}
+let title=document.querySelector('h1')
+function changeTitleColor(){
+    title.style.color="blue"
+}
+let text=document.querySelector('.card > p ')
+let text1=document.querySelector('.card > ul')
+function changeTextColor(){
+    text.style.color="green"
+    text1.style.color="green"
+}
+let imgborder=document.querySelector('img')
+function addBorder(){
+    imgborder.style.border="2px solid black"
+}
