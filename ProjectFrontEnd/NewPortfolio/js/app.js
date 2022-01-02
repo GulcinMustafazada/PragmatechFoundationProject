9efc75a3cb637d00eb949b90245d168aafd2d744
Gulcin Mustafazade <gulcinmustafazada@gmail.com>
2022-01-03 00:02:35 +0400

    some js codes added

--- ProjectFrontEnd/NewPortfolio/js/app.js
+++ ProjectFrontEnd/NewPortfolio/js/app.js
@@ -1,9 +1,11 @@
 let posX=0;
 let slides=document.querySelector('.slides')
 function changePageRight(){
-
+posX+=100;
+slides.style.left = `${pos}px`
 }
 function changePageLeft(){
-
+    posX-=100
+    slides.style.left = `${pos}px`
 }
 
