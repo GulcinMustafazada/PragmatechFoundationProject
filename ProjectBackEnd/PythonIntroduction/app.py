4059ac09de88d8c2ed8fd6924b5ff8fa0bad8191
Gulcin Mustafazade <gulcinmustafazada@gmail.com>
2022-01-29 15:28:10 +0400

    final task 01

--- ProjectBackEnd/PythonIntroduction/app.py
+++ ProjectBackEnd/PythonIntroduction/app.py
@@ -1,9 +1,30 @@
 #first exercise
-a=input()
-print(type(a))
+# a=int(input())
+# print(type(a))
 
-#third exercise
-b=str(input())
-x=list(b)
-x.reverse()
-print(x)
+# #third exercise
+# b=str(input())
+# x=list(b)
+# x.reverse()
+# print(x)
+
+#4-cu tapsiriw
+# c=str(input())
+# count=0
+# for i in c:
+#     if i==i.upper():
+#         count=count+i
+#     print(count)    
+
+
+# #list tapsiriqlari
+# arr=['Samir','Mehemmed','Qurbani','Vesile','Qurbaneli','Memmedaga','Nurlan','Leman','Emil','Gulshen']
+# count=0
+# for i in arr:
+#     count=count+arr[i]
+# print(count)
+# for i in range(100,500):
+#     sum=0
+#     sum=sum+i
+# print(sum)
+        
