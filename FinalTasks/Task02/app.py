bf9740feb8a01d2922de1c5e415f549586c5b272
Gulcin Mustafazade <gulcinmustafazada@gmail.com>
2022-02-02 17:43:42 +0400

    Task 02 kodlarında bəzi dəyişikliklər edildi

---
+++ FinalTasks/Task02/app.py
@@ -0,0 +1,9 @@
+x=input("Qeydiyyatdan kecmek ucun 1 yazin:")
+if x==1:
+    name=input("Istifadeci adiniz:")
+    parol=input("Parolunuz:")
+    print(name)
+    print(parol)
+y=input("Sisteme daxil olmaq ucun 2 yazin:")
+    
+       
