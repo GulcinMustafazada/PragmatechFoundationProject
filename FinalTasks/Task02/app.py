8ddd6f98c6dd40740818dcada720401ff66bdd3e
Gulcin Mustafazade <gulcinmustafazada@gmail.com>
2022-02-05 15:35:21 +0400

    Bəzi final task tapşırıqları edildi

--- FinalTasks/Task02/app.py
+++ FinalTasks/Task02/app.py
@@ -1,9 +1,38 @@
-x=input("Qeydiyyatdan kecmek ucun 1 yazin:")
-if x==1:
-    name=input("Istifadeci adiniz:")
-    parol=input("Parolunuz:")
-    print(name)
-    print(parol)
-y=input("Sisteme daxil olmaq ucun 2 yazin:")
-    
-       
+users=[]
+file=open('data.txt','a')
+class User:
+    def __init__(self,username,parol):
+        self.username=username
+        self.parol=parol
+        file.write(f'{self.username} ,{self.parol}/n')
+        
+    def ShowUserData(self):
+        print(f'{self.username} ,{self.parol}')
+        
+while True:
+    emr=input("Qeydiyattdan keçmək üçün 1 yazın:")
+    if emr=='1':
+        username=input("İstifadəçi adınızı daxil edin:")
+        parol=input("Parolunuzu daxil edin:")
+        user=User(username,parol)
+    elif emr=="2":
+        for user in users:
+            user.ShowUsersData()
+    else:
+        break
+emr1=input("Sistemə daxil olmaq üçün 2 yazın:")
+if emr1=='2':
+    print("siz sisteme daxil oldunuz")
+emr2=input("Ana menyuya qayitmaq üçün 3 yazın:")
+if emr=='3':
+     emr=input("Qeydiyattdan keçmək üçün 1 yazın:")
+     if emr=='1':
+        username=input("İstifadəçi adınızı daxil edin:")
+        parol=input("Parolunuzu daxil edin:")
+        user=User(username,parol)
+     elif emr=="2":
+        for user in users:
+            user.ShowUsersData()
+emr3=input("Ana ekrandan cixmaq ucun 4 yazin:")
+
+
